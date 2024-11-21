95e560bb24f92170ebf0b498a4110b91ac2f720f
Hasitha Adikari <adikari.adikari@mycit.ie>
2024-11-21 16:48:26 +0000

    download sentinal2 data set parallely

--- sentinal2.py
+++ sentinal2.py
@@ -5,10 +5,10 @@ import geopandas as gpd
 import os
 from sentinelsat import read_geojson
 from shapely.geometry import shape
+import multiprocessing
 
 copernicus_user = "adikari.adikari@mycit.ie" # copernicus User
 copernicus_password = "Hasitha@4805" # copernicus Password
-data_collection = "SENTINEL-2" # Sentinel satellite
 
 
 def get_polygon(path):
@@ -38,7 +38,59 @@ def get_keycloak(username: str, password: str) -> str:
         )
     return r.json()["access_token"]
 
-def download_data(start_date, end_date):
+def download_data(data_collection, area_foot_print, start_date, end_date):
+    print(f"Download data from data_collection: {data_collection} from  {start_date} to {end_date} for the area {area_foot_print}")
+    products_json = requests.get(
+        f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name eq '{data_collection}' "
+        f"and OData.CSC.Intersects(area=geography'SRID=4326;{area_foot_print}') and ContentDate/Start gt {start_date}T00:00:00.000Z "
+        f"and ContentDate/Start lt {end_date}T00:00:00.000Z&$count=True&$top=1000").json()
+
+    product_df = pd.DataFrame.from_dict(products_json['value'])
+    print(f"products_df : {product_df.shape}")
+    if product_df.shape[0]>0:
+        print(f"product_df info : {product_df.info()}")
+        product_df["geometry"] = product_df["GeoFootprint"].apply(shape)
+        geo_df = gpd.GeoDataFrame(product_df).set_geometry("geometry")  # Convert PD to GPD
+        print(f"geo_df : {geo_df}")
+        geo_df = geo_df[~geo_df["Name"].str.contains("L1C")]
+        geo_df["identifier"] = geo_df["Name"].str.split(".").str[0]
+        feature_count = len(geo_df)
+        if feature_count > 0:
+            try:
+                dir_path = f"data/{data_collection}/{today_string}"
+                os.makedirs(dir_path, exist_ok=True)
+                for i, feature in enumerate(geo_df.iterfeatures()):
+                    with multiprocessing.Pool() as pool:
+                        pool.map(download_image_zip, [(feature, dir_path)])
+            except Exception as e:
+                print(f"Problem with server error: {e}")
+        else:
+            print(f"Features not found|{feature_count}")
+    else:
+        print(f"No data found for the given criteria")
+
+def download_image_zip(file_info):
+    print(f"download_image_zip|file_info : {file_info}")
+    feature, dir_path = file_info
+    try:
+        session = requests.Session()
+        keycloak_token = get_keycloak(copernicus_user, copernicus_password)
+        session.headers.update({"Authorization": f"Bearer {keycloak_token}"})
+        url = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products({feature['properties']['Id']})/$value"
+        response = session.get(url, allow_redirects=False)
+        print(f"response : {response}")
+        while response.status_code in (301, 302, 303, 307):
+            url = response.headers["Location"]
+            response = session.get(url, allow_redirects=False)
+        print(feature["properties"]["Id"])
+        file = session.get(url, verify=False, allow_redirects=True)
+        with open(f"{dir_path}/{feature['properties']['identifier']}.zip", "wb", ) as p:
+            print(feature["properties"]["Name"])
+            p.write(file.content)
+    except Exception as e:
+        print(f"Problem with server error: {e}")
+
+def download_data1(start_date, end_date):
     ft = get_polygon('config/map.geojson')
     # https://catalogue.dataspace.copernicus.eu/odata/v1/DeletedProducts?$filter=Collection/Name%20eq%20%27SENTINEL-1%27%20and%20DeletionDate%20gt%202023-04-01T00:00:00.000Z%20and%20DeletionDate%20lt%202023-05-30T23:59:59.999Z&$orderby=DeletionDate&$top=20
     json_ = requests.get(
@@ -87,7 +139,9 @@ if __name__ == "__main__":
     today_string = today.strftime("%Y-%m-%d")
     yesterday = today - timedelta(days=1)
     yesterday_string = yesterday.strftime("%Y-%m-%d")
-    download_data(yesterday_string, today_string)
+    area_foot_print = get_polygon('config/map.geojson')
+    data_collection = "SENTINEL-2"  # Sentinel satellite
+    download_data(data_collection, area_foot_print, yesterday_string, today_string)
 
 
 
