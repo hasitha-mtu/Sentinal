786c887bdc590807d3ffa7fe2e08746298838b7a
Hasitha Adikari <adikari.adikari@mycit.ie>
2024-11-28 20:40:05 +0000

    re projected merged images

--- mosaic.py
+++ mosaic.py
@@ -7,6 +7,7 @@ import rasterio
 import rasterio.mask
 import rasterio.merge
 from rasterio.plot import show
+from rasterio.warp import calculate_default_transform, reproject, Resampling
 
 from pprint import pprint
 import matplotlib.pyplot as plt
@@ -15,39 +16,39 @@ from utils import get_parent_directories
 import glob
 
 
-def select_files(path, pattern):
-    L = []
-    for root, dirs, files in os.walk(path):
-        if len(dirs) == 0:
-            for f in files:
-                if re.match(pattern, f):
-                    L.append(os.path.join(root, f))
-    return L
+# def select_files(path, pattern):
+#     L = []
+#     for root, dirs, files in os.walk(path):
+#         if len(dirs) == 0:
+#             for f in files:
+#                 if re.match(pattern, f):
+#                     L.append(os.path.join(root, f))
+#     return L
+#
+#
+# def convert_to_tiff(paths):
+#     tiff_paths = []
+#     for p in paths:
+#         print("Converting " + p)
+#         with rasterio.open(p, mode="r") as src:
+#             profile = src.meta.copy()
+#             profile.update(driver="GTiff")
+#
+#             outfile = re.sub(".jp2", ".tiff", p)
+#             with rasterio.open(outfile, 'w', **profile) as dst:
+#                 dst.write(src.read())
+#                 tiff_paths.append(outfile)
+#     return tiff_paths
+#
+# def convert(DL_DIR):
+#     """
+#     Converting the .jp2 images to .tiff
+#     """
+#     jp2_paths = select_files(DL_DIR, "T*.*m.jp2$")
+#     tiff_paths = convert_to_tiff(jp2_paths)
+#     return tiff_paths
 
-
-def convert_to_tiff(paths):
-    tiff_paths = []
-    for p in paths:
-        print("Converting " + p)
-        with rasterio.open(p, mode="r") as src:
-            profile = src.meta.copy()
-            profile.update(driver="GTiff")
-
-            outfile = re.sub(".jp2", ".tiff", p)
-            with rasterio.open(outfile, 'w', **profile) as dst:
-                dst.write(src.read())
-                tiff_paths.append(outfile)
-    return tiff_paths
-
-def convert(DL_DIR):
-    """
-    Converting the .jp2 images to .tiff
-    """
-    jp2_paths = select_files(DL_DIR, "T*.*m.jp2$")
-    tiff_paths = convert_to_tiff(jp2_paths)
-    return tiff_paths
-
-def merge(tiff_paths, output_file, DEBUG):
+def merge_file(tiff_paths, output_file, DEBUG):
     """
     We're mergin the raster images.
     """
@@ -99,12 +100,56 @@ def perform_jp2_to_tiff_conversion(input_dir):
         for jp2_image in jp2_images:
             convert_jp2_to_tiff(jp2_image)
 
-# if __name__ == "__main__":
-#     today_string = date.today().strftime("%Y-%m-%d")
-#     collection_name = "SENTINEL-2"  # Sentinel satellite
-#     download_dir = f"data/{collection_name}/{today_string}"
-#     perform_jp2_to_tiff_conversion(download_dir)
+def re_project_file(image_path, output_file, dst_crs = 'EPSG:4326'):
+    """
+    Reprojecting the images to  EPSG:4326
+    """
+    print(f"reproject image_path : {image_path}")
+    print(f"reproject output_file : {output_file}")
+    with rasterio.open(image_path) as src:
+        transform, width, height = calculate_default_transform(
+            src.crs, dst_crs, src.width, src.height, *src.bounds
+        )
+        kwargs = src.meta.copy()
+        kwargs.update({
+                'crs': dst_crs,
+                'transform': transform,
+                'width': width,
+                'height': height
+        })
+        with rasterio.open(output_file, mode="w", **kwargs) as dst:
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
 
+
+def merge_files(download_dir, resolution, band_list, debug):
+    merged_dir = f"{download_dir}/merged"
+    os.makedirs(merged_dir, exist_ok=True)
+    for band in band_list:
+        tiff_path_list = []
+        for parent_dir in parent_dirs:
+            print(f"parent_dir : {parent_dir}")
+            tiff_path = f"{parent_dir}/converted/{band}_{resolution}m.tiff"
+            if os.path.isfile(tiff_path):
+                tiff_path_list.append(tiff_path)
+        print(f"Band {band}m tiff files : {tiff_path_list}")
+        merge_file(tiff_path_list, f"{merged_dir}/{band}_{resolution}m.tiff", debug)
+
+def re_project_files(download_dir, resolution,  dst_crs='EPSG:4326'):
+    reprojected_dir = f"{download_dir}/reprojected"
+    os.makedirs(reprojected_dir, exist_ok=True)
+    merged_dir = f"{download_dir}/merged"
+    merged_files = glob.glob(f"{merged_dir}/*_{resolution}m.tiff")
+    for merged_file in merged_files:
+        output_file = re.sub("merged", "reprojected", merged_file)
+        re_project_file(merged_file, output_file, dst_crs)
 
 if __name__ == "__main__":
     today_string = date.today().strftime("%Y-%m-%d")
@@ -115,16 +160,7 @@ if __name__ == "__main__":
     band_list = ['AOT', 'B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B11', 'B12', 'SCL',
                  'TCI', 'WVP']
     resolution = 10  # Define the target resolution (e.g., 10 meters)
-    for band in band_list:
-        tiff_path_list = []
-        for parent_dir in parent_dirs:
-            print(f"parent_dir : {parent_dir}")
-            tiff_path = f"{parent_dir}/converted/{band}_{resolution}m.tiff"
-            if os.path.isfile(tiff_path):
-                tiff_path_list.append(tiff_path)
-        print(f"Band {band}m tiff files : {tiff_path_list}")
-        merged_dir = f"{download_dir}/merged"
-        os.makedirs(merged_dir, exist_ok=True)
-        merge(tiff_path_list, f"{merged_dir}/{band}_{resolution}m.tiff", True)
+    merge_files(download_dir, resolution, band_list, False)
+    re_project_files(download_dir, resolution)
 
 
